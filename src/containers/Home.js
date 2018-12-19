d51461acaa922d6681b748f0a419bec235c71d9f
Hannah Stahl <hls62@cornell.edu>
2018-12-19 13:54:31 -0600

    Conditionally displaying Draft vs Published

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -45,7 +45,7 @@ export default class Home extends Component {
               <ListGroupItem header={blogPost.title}>
                 {blogPost.content.trim().split("\n")[0]}
                 <br/><br/>
-                {"Published: " + new Date(blogPost.createdAt).toLocaleString()}
+                {blogPost.blogPostState === "Published" ? "Published: " + new Date(blogPost.createdAt).toLocaleString() : "Draft"}
               </ListGroupItem>
             </LinkContainer>
           : <LinkContainer
